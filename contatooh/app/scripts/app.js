5241a97030dd36bc9a9b54240992a1480dd92fe4
Isaque Prestes Ferreira <isaquepferreira@hotmail.com>
2016-03-22 17:23:16 -0300

    commit changes

---
+++ contatooh/app/scripts/app.js
@@ -0,0 +1,14 @@
+'use strict';
+(function(){
+    var app = angular.module('contatoohApp', []);
+    app.config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
+}.call(this));
---
+++ contatooh/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('contatoohApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
