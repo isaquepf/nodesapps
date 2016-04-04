d77f0bf1ca00c7fa559d0f3e0b5f10aacda2e3a0
Isaque Prestes Ferreira <isaquepferreira@hotmail.com>
2016-04-03 23:06:49 -0300

    mongoose connection.

---
+++ contatooh/config/database.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+var connection;
+
+module.exports = function(uri) {
+
+    if (!connection) {
+        connection = mongoose.createConnection(uri);
+    }
+
+    mongoose.connection.on('connected', function() {
+        console.log('Mongoose! Conectado em ' + uri);
+    });
+
+    mongoose.connection.on('disconnected', function() {
+        console.log('Mongoose! Desconectado de ' + uri);
+    });
+
+    mongoose.connection.on('error', function(erro) {
+        console.log('Mongoose! Erro na conexão ' + erro);
+    });
+
+    return connection;
+};
